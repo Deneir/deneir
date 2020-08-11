f1871e0b3c3de06a038ab2c223b5e219cbc68a64
feelic <feelics@gmail.com>
2020-08-11 18:31:22 +0200

    feat(nodelist): design integration of node list with status colors

--- src/components/NodeList.js
+++ src/components/NodeList.js
@@ -1,39 +1,46 @@
-import React, { Fragment, useState } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import {
-  Link,
-} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCaretDown, faCaretRight } from '@fortawesome/free-solid-svg-icons';
-import StatusBubble from './StatusBubble';
+import { getConfig } from '../services/read-config';
+import { statusCodesToLabels } from '../constants/status-codes';
 import styles from './NodeList.module.scss';
 
 export default function NodeList(props) {
-  const {
-    title, nodes, defaultOpen = true,
-  } = props;
+  const { title, nodes, defaultOpen = true } = props;
+  const settings = getConfig('canvasSettings');
+  const { statusColors } = settings;
   const [open, setOpen] = useState(defaultOpen);
   const toggleIcon = (open && faCaretDown) || faCaretRight;
+  const aggregatedStatusCode = Math.max(...nodes.map((n) => n.status));
+  const statusLabel = statusCodesToLabels[aggregatedStatusCode];
+  const statusColor = statusColors[statusLabel];
 
   return (
-    <Fragment>
-      <h3 className={styles.listTitle} onClick={() => setOpen(!open)}>
+    <div className={styles.nodeList}>
+      <h3 style={{ borderColor: statusColor }} onClick={() => setOpen(!open)}>
         <FontAwesomeIcon icon={toggleIcon} /> {title}
       </h3>
       <ul className={(!open && styles.closed) || ''}>
         {nodes.length > 0
-          && nodes.map((item) => (
-            <li key={item.id}>
-              <Link className={styles.nodeLink} to={`/node/${item.id}`}>
-                <StatusBubble statusCode={item.status} /> {item.id}
-              </Link>
-            </li>
-          ))}
+          && nodes.map((item) => {
+            const itemStatusLabel = statusCodesToLabels[item.status];
+            const itemStatusColor = statusColors[itemStatusLabel];
+
+            return (
+              <li key={item.id} style={{ borderColor: itemStatusColor }}>
+                <Link className={styles.nodeLink} to={`/node/${item.id}`}>
+                  {item.id}
+                </Link>
+              </li>
+            );
+          })}
         {nodes.length === 0 && (
           <p className={styles.emptyListMessage}>there are 0 nodes in this list</p>
         )}
       </ul>
-    </Fragment>
+    </div>
   );
 }
 
